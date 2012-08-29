903fbdd147a09743051bf5d36abc2d8868e214eb
Stefan Ledin <stefan.ledin@gmail.com>
2012-08-29 20:54:42 +0200

    Second commit

--- app.rb
+++ app.rb
@@ -1,12 +1,58 @@
-#!/bin/env ruby
-# encoding: utf-8
-
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/app.db")
+
+class Note
+	include DataMapper::Resource
+	property :id, Serial
+	property :content, Text, :required => true
+	property :complete, Boolean, :required => true, :default => false
+	property :created_at, DateTime
+	property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
+
 get '/' do
-	@siteTitle = 'The Guestbook'
-	@message = 'Hello, is it me you´re looking for?'
+	@notes = Note.all :order => :id.desc
+	@title = 'All Notes'
 	erb :home
+end
+
+post '/' do
+	n = Note.new
+	n.content = params[:content]
+	n.created_at = Time.now
+	n.updated_at = Time.now
+	n.save
+	redirect '/'
+end
+
+get '/:id' do
+	@note = Note.get params[:id]
+	@title = "Edit note ##{params[:id]}"
+	erb :edit
+end
+
+put '/:id' do
+	n = Note.get params[:id]
+	n.content = params[:content]
+	n.complete = params[:complete] ? 1 : 0
+	n.updated_at = Time.now
+	n.save
+	redirect '/'
+end
+
+get '/:id/delete' do
+	@note = Note.get params[:id]
+	@title = "Confirm deletion of note ##{params[:id]}"
+	erb delete
+end
+
+delete '/:id' do
+	n = Note.get params[:id]
+	n.destroy
+	redirect '/'
 end
