dd8b703c44d544a0e58b29a17614224fa08008fc
stefanledin <stefan.ledin@gmail.com>
2012-09-06 13:46:09 +0200

    Shows and saves messages

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/app.db")
 
-class Note
+class Message
 	include DataMapper::Resource
 	property :id, Serial
-	property :content, Text, :required => true
-	property :complete, Boolean, :required => true, :default => false
+	property :name, Text, :required => true
+	property :message, Text, :required => true
 	property :created_at, DateTime
 	property :updated_at, DateTime
 end
@@ -16,43 +19,16 @@ end
 DataMapper.finalize.auto_upgrade!
 
 get '/' do
-	@notes = Note.all :order => :id.desc
-	@title = 'All Notes'
+	@messages = Message.all :order => :id.desc
+	@title = 'A Guestbook'
 	erb :home
 end
 
 post '/' do
-	n = Note.new
-	n.content = params[:content]
-	n.created_at = Time.now
-	n.updated_at = Time.now
-	n.save
-	redirect '/'
-end
+	m = Message.new
+	m.name = params[:name]
+	m.message = params[:message]
+	m.save
 
-get '/:id' do
-	@note = Note.get params[:id]
-	@title = "Edit note ##{params[:id]}"
-	erb :edit
-end
-
-put '/:id' do
-	n = Note.get params[:id]
-	n.content = params[:content]
-	n.complete = params[:complete] ? 1 : 0
-	n.updated_at = Time.now
-	n.save
-	redirect '/'
-end
-
-get '/:id/delete' do
-	@note = Note.get params[:id]
-	@title = "Confirm deletion of note ##{params[:id]}"
-	erb delete
-end
-
-delete '/:id' do
-	n = Note.get params[:id]
-	n.destroy
 	redirect '/'
 end
